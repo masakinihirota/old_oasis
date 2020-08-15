a1f8f4328094554e6dce6fe89bee1545a1e680a7
masakinihirota <masakinihirota@gmail.com>
2020-08-16 03:43:40 +0900

    Next.jsテンプレートインストール直後

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export const Home = (): JSX.Element => (
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Hello <a href="https://nextjs.org">Next.js!</a>
       </h1>
 
       <p className="description">
